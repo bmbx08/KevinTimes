cf26ab989b510a27ed67c4c781d250a9d61c395b
bmbx08 <bmbx08@bu.ac.kr>
2024-05-11 03:54:17 +0900

    fourth commit

--- main.js
+++ main.js
@@ -1,79 +1,97 @@
-const API_KEY = `a770d1855a244811978ee7f9c6834ef8`;
+const API_KEY = `a770d1855a244811978ee7f9c6834ef8`
 let newsList = [];
 // let category = "general";
-// let searchInput = document.getElementById("search-input");
+let searchInput = document.getElementById("search-input");
 let searchButton = document.getElementById("search-button");
 const menus = document.querySelectorAll(".menus button");
-menus.forEach(menu=>menu.addEventListener("click",(event)=>getNewsByCategory(event)))
+menus.forEach(menu => menu.addEventListener("click", (event) => getNewsByCategory(event)));
+let url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`);
+
 
 
 const getLatestNews = () => {
-  const url = new URL(
+  url = new URL(
     `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
   );
-  showNews(url);
+  showNews();
 };
 
-const getNewsByCategory= (event) => {
+const getNewsByCategory = (event) => {
   const category = event.target.textContent.toLowerCase(); //버튼의 값을 category 변수에 저장
   console.log("category", category);
-  const url = new URL(
+  url = new URL(
     `https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`
   );
-  showNews(url);
+  showNews();
 };
 
-const getNewsByKeyword = () => {
+const getNewsByKeyword = async () => {
   keyword = document.getElementById("search-input").value;
-  const url = new URL(
+  url = new URL(
     `https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`
   )
-  showNews(url);
-}
-
-const showNews = async (url) => {
-  const response  = await fetch(url);
+  const response = await fetch(url);
   const data = await response.json();
   newsList = data.articles;
   console.log(newsList);
-  render(); 
-}
+  render();
+  try {
+    if (newsList == 0) {
+      throw new Error("검색 결과 없음!");
+    }
+  } catch (error) {
+    let resultHTML = 
+      `<div class="alert alert-danger" role="alert">
+      ${error.message}
+      </div>`;
+    document.getElementById("news-board").innerHTML = resultHTML;
+  };
+};
 
-const render = () => {
-  // for (i = 0; i < newsList.length; i++) {
-  //   console.log("news", i, newsList[i].title, newsList[i].description);
-  // }
-  const newsHTML = newsList
-    .map(
-      (news) => `<div class="row news">    
+
+
+  const showNews = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    newsList = data.articles;
+    console.log(newsList);
+    render();
+  }
+
+  const render = () => {
+    // for (i = 0; i < newsList.length; i++) {
+    //   console.log("news", i, newsList[i].title, newsList[i].description);
+    // }
+    const newsHTML = newsList
+      .map(
+        (news) => `<div class="row news">    
             <div class="col-lg-4">
-                <img class="news-img-size" src=${
-                  news.urlToImage ||
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
-                }>
+                <img class="news-img-size" src=${news.urlToImage ||
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+          }>
             </div>
             <div class="col-lg-8">
                 <h2>${news.title}</h2>
                 <p>
-                    ${news.description||"내용 없음"}
+                    ${news.description || "내용 없음"}
                 </p> <br>
                 <div>
-                    ${news.source.name||"no source"} * ${moment(news.publishedAt).fromNow()}
+                    ${news.source.name || "no source"} * ${moment(news.publishedAt).fromNow()}
                 </div>
             </div>
         </div>`
-    )
-    .join("");
-  document.getElementById("news-board").innerHTML = newsHTML;
-};
+      )
+      .join("");
+    document.getElementById("news-board").innerHTML = newsHTML;
+  };
 
 
-// searchButton.addEventListener("click",searchNews);
-searchButton.addEventListener("click",function(){
-    searchInput.value=""
-});
+  // searchButton.addEventListener("click",searchNews);
+  searchButton.addEventListener("click", function () {
+    searchInput.value = ""
+  });
 
-getLatestNews();
+  getLatestNews();
 
 
 // function render() {
